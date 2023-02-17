d16b2a716e31026b6003424c47844fa7b300b777
Toni Vukovic <tonivukovic1204@gmail.com>
2023-02-17 18:12:32 +0100

    add arrows

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 let menu = document.getElementById("menu");
 let tipka = document.getElementById("tipka");
+let gore = document.getElementById("up");
+let lijevo = document.getElementById("left");
 
 function toggleOpen() {
   menu.classList.toggle("open");
   tipka.classList.toggle("active");
+  gore.classList.toggle("show");
+  lijevo.classList.toggle("show-left");
 }
 
 window.onclick = function (e) {
@@ -11,6 +15,8 @@ window.onclick = function (e) {
     if (menu.classList.contains("open") && tipka.classList.contains("active")) {
       menu.classList.remove("open");
       tipka.classList.remove("active");
+      gore.classList.remove("show");
+      lijevo.classList.remove("show-left");
     }
   }
 };
